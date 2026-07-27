4c9b0bb7ef3ae4e16efa105fd9a30429ea97a62b
Matt Fisher <mfisher87@gmail.com>
2026-07-27 15:51:09 -0600

    Replace theme section with 2026 theme

--- src/components/themes.tsx
+++ src/components/themes.tsx
@@ -5,25 +5,15 @@ export function ThemesSection() {
   return (
     <Section dark id="themes">
       <LeftColumn>
-        <SectionTitle>Themes</SectionTitle>
+        <SectionTitle>Theme</SectionTitle>
       </LeftColumn>
       <RightColumn>
-        <SubSectionTitle>Innovation & Disruption</SubSectionTitle>
+        <SubSectionTitle>Last Mile of Geo</SubSectionTitle>
         <List>
-          <Item>We are in a time of high velocity change in hardware and software innovation</Item>
-          <Item>Technology, particularly advancements in generative AI, is moving quickly.</Item>
-          <Item>How do we, as a community, adapt to and harness innovation in times of disruption?</Item>
+          <Item>What is needed to take our geospatial work that "last mile" to make a meaningful impact?</Item>
+          <Item>In your sector, what does the "last mile" look like?</Item>
+          <Item>Why is the "last mile" hard, and why do we sometimes lose sight of it?</Item>
         </List>
-        <SubSectionTitle>Climate Change Mitigation & Adaptation</SubSectionTitle>
-        <List>
-          <Item>Climate Change is the existential threat facing our generation.</Item>
-          <Item>The impacts of Climate Change are accelerating.</Item>
-          <Item>Where are we rising to the challenge, and where are we falling behind?</Item>
-        </List>
-        <SubSectionTitle>Social Cohesion & Grassroots Action</SubSectionTitle>
-          <Item>How do we foster new collaborations and strengthen bonds in a time of polarization?</Item>
-          <Item>What efforts demand our energy and how do we find the resilience to try?</Item>
-          <Item>How do we ensure that the benefits of our work are shared equitably?</Item>
       </RightColumn>
     </Section>
   );
