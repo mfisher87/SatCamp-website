6a9f1ff1d3490ba0dca89385b59700e16513bad6
Matt Fisher <mfisher87@gmail.com>
2026-07-27 18:26:26 -0600

    Add panel titles to agenda

--- src/components/agenda.tsx
+++ src/components/agenda.tsx
@@ -26,7 +26,7 @@ export function AgendaSection() {
           <Item>8:30am Breakfast (provided)</Item>
           <Item>9:00am Outdoor Activities</Item>
           <Item>1:00pm Lunch (provided)</Item>
-          <Item>2:00pm Panel 1</Item>
+          <Item>2:00pm Panel: The Last Mile of Geo</Item>
           <Item>3:15pm Break</Item>
           <Item>3:45pm Presentations</Item>
           <Item>4:45pm Small Group Activity</Item>
@@ -37,7 +37,7 @@ export function AgendaSection() {
           <LocationDesc>Grand Assembly Hall</LocationDesc>
           <Item>8:00am Cabin Checkout (deadline 10:30am)</Item>
           <Item>8:00am Breakfast (provided)</Item>
-          <Item>9:00am Panel 2</Item>
+          <Item>9:00am Panel: Where's your line? Exploring Alignment of Personal Ethics with Dual-Use Geospatial Technology</Item>
           <Item>10:00am Outdoor Activities</Item>
           <Item>1:00pm Lunch (provided)</Item>
           <Item>2:00pm Lightning Talks Round 2</Item>
